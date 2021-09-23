cfdef3323d13bf11257b628492e95f901977836c
sanatech95 <tasneem786786.khan@gmail.com>
2021-09-23 11:06:01 +0530

    first commit

---
+++ src/main/java/com/springrest/springrest/SpringbootAngularAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springrest.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springrest/springrest/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.springrest.springrest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.OK)
+public class ResourceNotFoundException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+	
+	
+
+}
